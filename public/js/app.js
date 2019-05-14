99d0928cfd516590e89fa36baf92d619f0bc623a
Saurabh Sharma <saurabh.sharma06@nagarro.com>
2019-05-14 22:17:49 +0530

    some changes

--- public/js/app.js
+++ public/js/app.js
@@ -4,8 +4,6 @@ const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
 const messageTwo = document.querySelector('#message-2');
 
-// messageOne.textContent = 'Desi';
-
 
 weatherForm.addEventListener('submit', (e)=> {
   e.preventDefault()
