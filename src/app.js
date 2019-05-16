c22acad19c1edbddd2341929c25537326730796d
Saurabh Sharma <jawas.saurabh@gmail.com>
2019-05-16 23:14:28 +0530

    changed about me page

--- src/app.js
+++ src/app.js
@@ -25,14 +25,14 @@ app.use(express.static(publicDirectoryPath));
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Andrew Mead'
+        name: 'Bulla Jackson'
     })
 });
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Andrew Mead'
+        name: 'Bulla Jackson'
     })
 });
 
@@ -40,7 +40,7 @@ app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text.',
         title: 'Help',
-        name: 'Andrew Mead'
+        name: 'Bulla Jackson'
     })
 });
 
@@ -118,7 +118,7 @@ app.get('/help/*', (req,res)=> {
   res.render('404',{
     title: '404',
     errorMsg: 'Help article not found',
-    name: 'Bulla'
+    name: 'Bulla Jackson'
   })
 });
 
@@ -127,7 +127,7 @@ app.get('*', (req, res)=> {
   res.render('404',{
     title: '404',
     errorMsg: 'Page not Found',
-    name: 'Andrew Mead'
+    name: 'Bulla Jackson'
   })
 })
 
