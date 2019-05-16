f2b62060db9926af1f749ac8cdef60f70018b6aa
Saurabh Sharma <jawas.saurabh@gmail.com>
2019-05-16 22:51:42 +0530

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,7 @@ weatherForm.addEventListener('submit', (e)=> {
 
   // console.log(address);
 
-  fetch('http://localhost:3000/weather?address=' + address).then((response)=> {
+  fetch('/weather?address=' + address).then((response)=> {
     response.json().then((data)=> {
       // console.log(data);
 
